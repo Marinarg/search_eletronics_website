37dfd4e7e632100ae65fedfb72ddc00c9a94cc81
Marinarg <marinarg_09@hotmail.com>
2021-12-12 21:30:24 -0300

    analysis

--- src/components/AnalysisModal.vue
+++ src/components/AnalysisModal.vue
@@ -1,6 +1,6 @@
 <template>
 	<n-modal v-model:show="showModal" @close="(value)=>$emit('update:showModal', value)" 
-		@keyup.esc="$emit('update:showModal', false)">
+		@keyup.esc="showModal=false">
 		<n-card>
 			{{JSON.stringify(item)}}
 		</n-card>
