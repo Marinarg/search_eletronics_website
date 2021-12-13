f50ea8406598e885eb042a0927d366299ea6368d
Marinarg <marinarg_09@hotmail.com>
2021-12-12 21:16:00 -0300

    analysis

---
+++ src/components/AnalysisModal.vue
@@ -0,0 +1,26 @@
+<template>
+	<n-modal v-model:show="showModal">
+		<n-card>
+			{{JSON.stringify(item)}}
+		</n-card>
+	</n-modal>
+</template>
+<script>
+import {NModal, NCard} from 'naive-ui/'
+
+export default {
+	name: "AnalysisModal",
+	components: {NModal,NCard},
+	props: {
+		showModal: {
+			required: true,
+			type: Boolean,
+		},
+		item: {
+			required: false,
+			type: undefined
+		}
+	}
+}
+
+</script>
