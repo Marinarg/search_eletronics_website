e0f099652df1eadfe763e940c18135470ddb7455
Marinarg <marinarg_09@hotmail.com>
2022-01-15 15:07:14 -0300

    add button recomendations

--- src/components/AnalysisModalContent.vue
+++ src/components/AnalysisModalContent.vue
@@ -1,9 +1,26 @@
 <template>
 	<n-card style="width:600px; height:400px;">
 		<div style="display: flex; flex-direction: column; align-items: flex-start; margin-right: 0px; padding: 1rem">
-			<span class="recommendations" >
-				Recomendações: {{results_recommendations}}
-			</span>
+			<table class="recommendations">
+					<thead class="tableHeader">
+						<span>
+							Pessoas que buscaram esse produto também se interessaram por:
+						</span>
+					</thead>
+					<tbody>
+						<template v-for="value in results_recommendations">
+							<tr class="row" data-index=1 style="background-color: white">
+								<td class="item-name">
+									<div style="display: flex; flex-direction: column; align-items: flex-start; margin-right: 0px; padding: 1rem">
+										<span class="product-name-text" >
+											{{value}}
+										</span>
+									</div>
+								</td>
+							</tr>
+						</template>
+					</tbody>
+				</table>
 		</div>
 	</n-card>
 </template>
